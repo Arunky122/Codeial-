a801b8d45cc33bc1ad9c46b1e4e16858ec2b1377
Arunky122 <arunky122@gmail.com>
2020-04-26 17:25:08 +0530

    add comments to the post

---
+++ controller/comments_controller.js
@@ -0,0 +1,29 @@
+const Comment = require('../models/comments');
+const Post = require('../models/post');
+
+
+module.exports.create = (req,res)=>{
+    Post.findById(req.body.post, (err,post)=>{
+
+        if(post){
+            Comment.create({
+                content:req.body.content,
+                post:req.body.post,
+                user:req.user._id
+            }, (err,comment)=>{
+
+                if(err){
+                    console.log("Error in creating the comment");
+                    return;
+                }
+               
+
+                post.comments.push(comment);
+                post.save();
+
+                res.redirect('/');
+            });
+        }
+    });
+
+}
