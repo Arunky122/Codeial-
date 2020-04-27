b027e95b50cc5b97ec5998eed00e9441d0e291d5
Arunky122 <arunky122@gmail.com>
2020-04-27 21:43:52 +0530

    add delete function to the comments and using partials for home.ejs

--- controller/comments_controller.js
+++ controller/comments_controller.js
@@ -2,21 +2,21 @@ const Comment = require('../models/comments');
 const Post = require('../models/post');
 
 
-module.exports.create = (req,res)=>{
-    Post.findById(req.body.post, (err,post)=>{
+module.exports.create = (req, res) => {
+    Post.findById(req.body.post, (err, post) => {
 
-        if(post){
+        if (post) {
             Comment.create({
-                content:req.body.content,
-                post:req.body.post,
-                user:req.user._id
-            }, (err,comment)=>{
+                content: req.body.content,
+                post: req.body.post,
+                user: req.user._id
+            }, (err, comment) => {
 
-                if(err){
+                if (err) {
                     console.log("Error in creating the comment");
                     return;
                 }
-               
+
 
                 post.comments.push(comment);
                 post.save();
@@ -26,4 +26,30 @@ module.exports.create = (req,res)=>{
         }
     });
 
+}
+
+module.exports.destroy = (req, res) => {
+    Comment.findById(req.params.id, async function (err, comment) {
+        if (comment.user == req.user.id) {
+            let postId = comment.post;
+            comment.remove();
+
+            Post.findByIdAndUpdate(postId, { $pull: { comments: req.params.id } }, (err, post) => {
+                return res.redirect('back');
+            });
+        
+        } else {
+            let comment = await Comment.findById(req.params.id).populate('post').exec();
+            if (comment.post.user == req.user.id) {
+                let postId = comment.post.id;
+                comment.remove();
+
+                Post.findByIdAndUpdate(postId, { $pull: { comments: req.params.id } }, (err, post) => {
+                    return res.redirect('back');
+                });
+            } else {
+                return res.redirect('back');
+            }
+        }
+    });
 }
