a52fd65c4a2f2172d248bf817ae410e40767ab12
Arunky122 <arunky122@gmail.com>
2020-05-02 16:34:58 +0530

    convert creating post into ajax

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -7,6 +7,7 @@ module.exports.home = async function(req,res){
 
         // Populate the user 
         let posts = await Post.find({})
+        .sort('-createdAt')
         .populate('user')
         .populate({
             path:'comments',
