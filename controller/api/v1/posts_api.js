c6196952c47ba447db37cb6c24121cc45af4c797
Arunky122 <arunky122@gmail.com>
2020-05-06 19:01:30 +0530

    add passport jwt strategy to the code

---
+++ controller/api/v1/posts_api.js
@@ -0,0 +1,49 @@
+const Post = require('../../../models/post');
+const Comment = require('../../../models/comments');
+module.exports.index = async function(req,res){
+
+    let posts = await Post.find({})
+        .sort('-createdAt')
+        .populate('user')
+        .populate({
+            path:'comments',
+            populate:{
+                path:'user'
+            }
+        });
+
+
+    return res.json(200,{
+        message:"List of post",
+        posts:posts
+    });
+}
+
+module.exports.destroy = async (req,res)=>{
+
+    try{
+        let post = await Post.findById(req.params.id);
+    
+       // if(post.user == req.user.id){
+            post.remove();
+
+        await Comment.deleteMany({post : req.params.id});
+       
+       return res.json(200,{
+           message:"Post and associated comments deleted successfully !"
+       });
+    }
+        // else{
+        //     req.flash('success','You cannot delete that post !');
+        //     return res.redirect('back');
+        // }
+
+
+    catch(err){
+        console.log("*****",err);
+        return res.json(500,{
+            message:"Internal Server Error !"
+        })
+    }
+
+}    
