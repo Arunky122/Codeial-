9ead976a47eed29174a35303366b4a73b80c0f8c
Arunky122 <arunky122@gmail.com>
2020-05-07 17:24:38 +0530

    add jwt to the project

--- config/passport-jwt-strategy.js
+++ config/passport-jwt-strategy.js
@@ -7,12 +7,12 @@ const ExtractJWT = require('passport-jwt').ExtractJwt;
 const User = require('../models/user');
 
 let opts={
-    jwtFromRequest : ExtractJWT.fromAuthHeaderAsBearerToken,
+    jwtFromRequest : ExtractJWT.fromAuthHeaderAsBearerToken(),
     secretOrKey:"codeial"
 }
 
 passport.use(new JWTStrategy(opts,function(jwtPayload,done){
-    User.findById(jwtPayload_.id, function(err,user){
+    User.findById(jwtPayload._id, function(err,user){
         if(err){
             console.log("Error in finding user from JWT");
             return;
--- controller/api/v1/posts_api.js
+++ controller/api/v1/posts_api.js
@@ -24,22 +24,23 @@ module.exports.destroy = async (req,res)=>{
     try{
         let post = await Post.findById(req.params.id);
     
-       // if(post.user == req.user.id){
+       if(post.user == req.user.id){
             post.remove();
 
-        await Comment.deleteMany({post : req.params.id});
+            await Comment.deleteMany({post : req.params.id});
        
-       return res.json(200,{
-           message:"Post and associated comments deleted successfully !"
-       });
-    }
-        // else{
-        //     req.flash('success','You cannot delete that post !');
-        //     return res.redirect('back');
-        // }
+            return res.json(200,{
+                message:"Post and associated comments deleted successfully !"
+            });
+        }
+        else{
+            return res.json(401,{
+                message:"You cannot delete this post !"
+            });
+        }
 
 
-    catch(err){
+    }catch(err){
         console.log("*****",err);
         return res.json(500,{
             message:"Internal Server Error !"
